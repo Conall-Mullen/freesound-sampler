bab2c0ee17d439ba12313e2099337c0f0391f429
Conall O Maolain <conallomaolain1@gmail.com>
2024-06-12 13:28:06 +0200

    add sign in button via github

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
+import LoginButton from "@/components/LoginButton";
 import Sampler from "@/components/Sampler";
 import Search from "@/components/Search";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
+  const { data: session, status } = useSession();
   return (
     <>
+      <LoginButton />
       <Sampler />
       <Search />
     </>
