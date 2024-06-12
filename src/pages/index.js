f3c6adb04e348163a660c15c2835fb05fec04777
Conall O Maolain <conallomaolain1@gmail.com>
2024-06-12 17:44:47 +0200

    create user in database when signing in with github

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
-import LoginButton from "@/components/LoginButton";
-import Sampler from "@/components/Sampler";
-import Search from "@/components/Search";
+import LoginButton from "../components/LoginButton";
+import Sampler from "../components/Sampler";
+import Search from "../components/Search";
 import { useSession } from "next-auth/react";
 
 export default function Home() {
   const { data: session, status } = useSession();
   return (
     <>
+      <h1>Freesound Sampler</h1>
       {!session && <LoginButton />}
       {session && (
         <>
