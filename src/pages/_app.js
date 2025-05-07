b3c4f726944fad196a46d25a25ebdc8892731f35
Conall O Maolain <conallomaolain1@gmail.com>
2025-05-07 15:45:42 +0200

    using chakra ui components

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,24 +1,24 @@
 import "../styles/globals.css";
+import { Provider } from "../components/ui/provider";
 import { SessionProvider } from "next-auth/react";
 import { SWRConfig } from "swr";
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
-})
-
-{
+}) {
   return (
-    <SWRConfig
-      value={{
-      
-        fetcher: (resource, init) =>
-          fetch(resource, init).then((res) => res.json()),
-      }}
-    >
-      <SessionProvider session={session}>
-        <Component {...pageProps} />
-      </SessionProvider>
-    </SWRConfig>
+    <Provider>
+      <SWRConfig
+        value={{
+          fetcher: (resource, init) =>
+            fetch(resource, init).then((res) => res.json()),
+        }}
+      >
+        <SessionProvider session={session}>
+          <Component {...pageProps} />
+        </SessionProvider>
+      </SWRConfig>
+    </Provider>
   );
 }
